4563a18205150b4690e37715ea6a9e215cfe7b75
David Peer <david.peer@microfocus.com>
2019-07-28 13:48:58 +0300

    Added env vars

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const path = require('path');
-const express = require('express');
+const os = require('os');
 const winston = require('winston');
+const express = require('express');
 
 const myFormat = winston.format.printf(({ level, message, timestamp }) => {
     return `${timestamp} ${level}: ${message}`;
 });
 
-// todo: change to env param / docker volume
-const filePath = path.join(process.cwd(), `alerts_${process.pid}_${Date.now()}.log`);
+const loggerFileName = `webhook-alerts_${process.pid}_${Date.now()}.log`;
+const loggerFilePath = process.env.LOGGER_PATH ? path.join(process.env.LOGGER_PATH, loggerFileName) : path.join(os.tmpdir(), loggerFileName);
 const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.timestamp(),
@@ -15,7 +16,24 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.File({
-            filename: filePath ,
+            filename: loggerFilePath ,
+            maxsize: 1024 * 1024 * 10,
+            maxFiles: 10,
+            tailable: true,
+        }),
+        new winston.transports.Console()
+    ]
+});
+
+const alertsFilePath = process.env.ALERTS_FILE_PATH ? process.env.ALERTS_FILE_PATH : path.join(process.cwd(), 'alerts.log');
+const alertsLogger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        myFormat
+    ),
+    transports: [
+        new winston.transports.File({
+            filename: alertsFilePath ,
             maxsize: 1024 * 1024 * 10,
             maxFiles: 10,
             tailable: true,
@@ -25,14 +43,33 @@ const logger = winston.createLogger({
 });
 
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
+
+let grafanaAlertsCntr = 0;
+let promethuesAlertsCntr = 0;
 
 app.use(express.json({strict: true, type: "application/json"}));
 
-app.post('/alert', (req, res) => {
-    logger.info(JSON.stringify(req.body));
+app.get('status', (req, res) => {
+    res.json({
+        grafanaAlerts: grafanaAlertsCntr,
+        promethuesAlerts: promethuesAlertsCntr
+    });
+});
+
+app.post('/grafana/alerts', (req, res) => {
+    alertsLogger.info(JSON.stringify(req.body));
+    grafanaAlertsCntr++;
     res.sendStatus(200);
 });
 
-app.listen(port/*,
-    () => console.log(`Example app listening on port ${port}!`)*/);
+app.post('/promethues/alerts', (req, res) => {
+    alertsLogger.info(JSON.stringify(req.body));
+    promethuesAlertsCntr++;
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    logger.info(`Listening on port: ${port}`);
+    logger.info(`Alerts file path: ${alertsFilePath}`);
+});
